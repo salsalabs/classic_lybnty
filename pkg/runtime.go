9df8e6a05255545c0ad905b38d6edba6ae41c037
Allen Leonard <aleonard@salsalabs.com>
2019-12-27 12:22:51 -0600

    Runtime in its own source.

---
+++ pkg/runtime.go
@@ -0,0 +1,97 @@
+package lybnty
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	godig "github.com/salsalabs/godig/pkg"
+)
+
+//Runtime contains the data necessary to run the application.
+type Runtime struct {
+	LastStart time.Time
+	LastEnd   time.Time
+	ThisStart time.Time
+	ThisEnd   time.Time
+	Org       string
+	API       *godig.API
+}
+
+//NewRuntime build the runtime environment for this app.
+func NewRuntime(a *godig.API, org *string, lastStart *string, lastEnd *string, thisStart *string, thisEnd *string) (r Runtime, err error) {
+	b := []*string{
+		org,
+		lastStart,
+		lastEnd,
+		thisStart,
+		thisEnd,
+	}
+	e := false
+	for i, v := range b {
+		fmt.Printf("checking parameter %v\n", *v)
+		if v == nil || len(*v) == 0 {
+			switch i {
+			case 0:
+				log.Println("--org is a required parameter")
+				e = true
+			case 1:
+				log.Println("--last-year-start is a required parameter")
+				e = true
+			case 2:
+				log.Println("--last-year-end is a required parameter")
+				e = true
+			case 3:
+				log.Println("--this-year-start is a required parameter")
+				e = true
+			case 4:
+				log.Println("--this-year-end is a required parameter")
+				e = true
+			}
+		}
+	}
+	if !e {
+		b := b[1:5]
+		for i, v := range b {
+			d1, err := time.Parse(godig.DateFormat, *v)
+			switch i {
+			case 0:
+				if err != nil {
+					log.Println("--last-year-start must be formatted as YYYY-MM-DD!")
+					e = true
+				} else {
+					r.LastStart = d1
+				}
+			case 1:
+				if err != nil {
+					log.Println("--last-year-end must be formatted as YYYY-MM-DD!")
+					e = true
+				} else {
+					r.LastEnd = d1
+				}
+			case 2:
+				d1, err := time.Parse(godig.DateFormat, *v)
+				if err != nil {
+					log.Println("--this-year-start must be formatted as YYYY-MM-DD!")
+					e = true
+				} else {
+					r.ThisStart = d1
+				}
+			case 3:
+				if err != nil {
+					log.Println("--this-year-end must be formatted as YYYY-MM-DD!")
+					e = true
+				} else {
+					r.ThisEnd = d1
+				}
+			}
+		}
+	}
+	if e {
+		err := fmt.Errorf("%s", "Unable to continue due to parameter errors")
+		return r, err
+	}
+	r.API = a
+	r.Org = *org
+	return r, nil
+}
