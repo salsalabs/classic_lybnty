ba8b5ab8e55aa61ae522424bd5b6b68219448073
Allen Leonard <aleonard@salsalabs.com>
2019-12-27 12:23:13 -0600

    Runtime in its own source.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	lybnty "github.com/salsalabs/classic_lybnty/pkg"
 	godig "github.com/salsalabs/godig/pkg"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -13,94 +14,6 @@ import (
 //Application to create a Last Year But Not This Year  (LYBNTY) report for Salsa Classic.
 //The user gets to define the start and end of both "Last Year" and "This Year", making this
 //more of a general-purpose report than a typicaly LYBUNTY report.
-
-//Runtime contains the data necessary to run the application.
-type Runtime struct {
-	LastStart time.Time
-	LastEnd   time.Time
-	ThisStart time.Time
-	ThisEnd   time.Time
-	Org       string
-	API       *godig.API
-}
-
-//NewRuntime build the runtime environment for this app.
-func NewRuntime(a *godig.API, org *string, lastStart *string, lastEnd *string, thisStart *string, thisEnd *string) (r Runtime, err error) {
-	b := []*string{
-		org,
-		lastStart,
-		lastEnd,
-		thisStart,
-		thisEnd,
-	}
-	e := false
-	for i, v := range b {
-		fmt.Printf("checking parameter %v\n", *v)
-		if v == nil || len(*v) == 0 {
-			switch i {
-			case 0:
-				log.Println("--org is a required parameter")
-				e = true
-			case 1:
-				log.Println("--last-year-start is a required parameter")
-				e = true
-			case 2:
-				log.Println("--last-year-end is a required parameter")
-				e = true
-			case 3:
-				log.Println("--this-year-start is a required parameter")
-				e = true
-			case 4:
-				log.Println("--this-year-end is a required parameter")
-				e = true
-			}
-		}
-	}
-	if !e {
-		b := b[1:5]
-		for i, v := range b {
-			d1, err := time.Parse(godig.DateFormat, *v)
-			switch i {
-			case 0:
-				if err != nil {
-					log.Println("--last-year-start must be formatted as YYYY-MM-DD!")
-					e = true
-				} else {
-					r.LastStart = d1
-				}
-			case 1:
-				if err != nil {
-					log.Println("--last-year-end must be formatted as YYYY-MM-DD!")
-					e = true
-				} else {
-					r.LastEnd = d1
-				}
-			case 2:
-				d1, err := time.Parse(godig.DateFormat, *v)
-				if err != nil {
-					log.Println("--this-year-start must be formatted as YYYY-MM-DD!")
-					e = true
-				} else {
-					r.ThisStart = d1
-				}
-			case 3:
-				if err != nil {
-					log.Println("--this-year-end must be formatted as YYYY-MM-DD!")
-					e = true
-				} else {
-					r.ThisEnd = d1
-				}
-			}
-		}
-	}
-	if e {
-		err := fmt.Errorf("%s", "Unable to continue due to parameter errors")
-		return r, err
-	}
-	r.API = a
-	r.Org = *org
-	return r, nil
-}
 
 func main() {
 
@@ -131,7 +44,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Authentication error: %+v\n", err)
 	}
-	rt, err := NewRuntime(a, org, lastStart, lastEnd, thisStart, thisEnd)
+	rt, err := lybnty.NewRuntime(a, org, lastStart, lastEnd, thisStart, thisEnd)
 	if err != nil {
 		log.Fatal(err)
 	}
